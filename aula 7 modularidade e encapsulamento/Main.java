d13065632bac4d1d54bd6d78df7a85c0eaeef21d
Gustavo <guslnhm@gmail.com>
2025-02-02 20:37:00 -0300

    evolucao exercicio modularidade e encapsulamento

--- aula 7 modularidade e encapsulamento/Main.java
+++ aula 7 modularidade e encapsulamento/Main.java
@@ -8,5 +8,25 @@ public class Main{
 		Aluno a2;
 		a2 = t1.pesquisarAluno(20010416);
 		//System.out.println(a2); //imprime a referência
+		
+		Turma t2 = new Turma(1); //originalmente 45
+		t2.setDisciplina("ALGORITMO E PROGRAMACAO DE COMPUTADORES");
+		t2.setCodigo(146);
+		Turma.listar_turmas();
+		
+		Turma.remover_turma(t1);
+		Turma.listar_turmas();
+		
+		Turma.buscar_turma(177);
+		Turma.buscar_turma(146);
+		
+		System.out.println("-------------------------------------");
+		
+		System.out.println(t2.getTotalVagas());
+		t2.matricularAluno(a1);
+		System.out.println(t2.getVagasDisponiveis());
+		
+		Aluno a3 = new Aluno("Mel", 20221105, "mel@unb.br");
+		t2.matricularAluno(a3);
 	}
 }
--- aula 7 modularidade e encapsulamento/Turma.java
+++ aula 7 modularidade e encapsulamento/Turma.java
@@ -10,8 +10,28 @@ public class Turma{
     
     private List<Aluno> alunos = new ArrayList<>();
     
-    public Turma(int qteVagas){
+    private static List<Turma> turmas = new ArrayList<>();
+    
+    public Turma(int qteVagas){ //CREATE
         totalVagas = qteVagas;
+        vagasDisponiveis = qteVagas;
+        turmas.add(this);
+    }
+    
+    public int getTotalVagas(){
+        return totalVagas;
+    }
+    
+    public int getVagasDisponiveis(){
+        return vagasDisponiveis;
+    }
+    
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public String getDisciplina(){
+        return disciplina;
     }
     
     public void setDisciplina(String disciplina){
@@ -27,6 +47,10 @@ public class Turma{
     }
     
     public boolean matricularAluno(Aluno aluno){
+        if(this.vagasDisponiveis==0){
+            System.out.println("ERRO: NAO FOI POSSIVEL MATRICULAR O ALUNO. TURMA LOTADA.");
+            return false;
+        }
         for(Aluno a: alunos){
             if(aluno.getMatricula() == a.getMatricula()){
                 System.out.println("ERRO: ALUNO JA MATRICULADO.");
@@ -35,6 +59,7 @@ public class Turma{
         }
         alunos.add(aluno);
         System.out.println("ALUNO MATRICULADO COM SUCESSO.");
+        this.vagasDisponiveis--;
         return true;
     }
     
@@ -59,4 +84,78 @@ public class Turma{
         return null;
     }
     
+    @Override
+    public String toString(){
+        return "    NOME DA DISCIPLINA: " + disciplina;
+    }
+    
+    public static void listar_turmas(){ //READ
+        System.out.println("LISTA DE OFERTA DE TURMAS:");
+        for(Turma t: turmas){
+            System.out.println(t);
+        }
+        System.out.println();
+    }
+    
+    public static boolean buscar_turma(int codigo){ //READ PT 2
+        for(Turma t: turmas){
+            if(t.codigo==codigo){
+                System.out.println("TURMA ENCONTRADA.");
+                System.out.println(t);
+                System.out.println();
+                return true;
+            }
+        }
+        System.out.println("ERRO: TURMA NAO ENCONTRADA." + "\n");
+        return false;
+    }
+    
+    public static boolean remover_turma(Turma turma){ //DELETE
+        for(Turma t: turmas){
+            if(t.codigo==turma.codigo){
+                System.out.println("TURMA REMOVIDA COM SUCESSO." + "\n");
+                turmas.remove(t);
+                return true;
+            }
+        }
+        System.out.println("ERRO: NAO FOI POSSIVEL REMOVER A TURMA ESPECIFICADA.");
+        return false;
+    }
+    
+    //UPDATE COM SOBRECARGA DE MÉTODO:
+    public void atualizarTurma(String disciplina, int codigo, int totalVagas){
+        this.disciplina = disciplina;
+        this.codigo = codigo;
+        this.totalVagas = totalVagas;
+        System.out.println("TURMA ATUALIZADA COM SUCESSO.\n");
+    }
+    
+    public void atualizarTurma(String disciplina, int codigo){
+        this.disciplina = disciplina;
+        this.codigo = codigo;
+        System.out.println("TURMA ATUALIZADA COM SUCESSO.\n");
+    }
+    
+    public void atualizarTurma(int codigo, int totalVagas){
+        this.codigo = codigo;
+        this.totalVagas = totalVagas;
+        System.out.println("TURMA ATUALIZADA COM SUCESSO.\n");
+    }
+    
+    public void atualizarTurma(String disciplina){
+        this.disciplina = disciplina;
+        System.out.println("TURMA ATUALIZADA COM SUCESSO.\n");
+    }
+    
+    public void atualizarTurma(int totalVagas){
+        this.totalVagas = totalVagas;
+        System.out.println("TURMA ATUALIZADA COM SUCESSO.\n");
+    }
+    
+    /*
+    PROBLEMA: COMPILADOR INTERPRETA type funcao(String, int) COMO IGUAL, OU SEJA
+    NÃO POSSO FAZER A SOBRECARGA DE MÉTODO PARA ATUALIZAR A TURMA
+    COM DISCIPLINA E CÓDIGO, E DEPOIS COM DISCIPLINA E TOTAL DE VAGAS, POR EXEMPLO.
+    COMO CONTORNAR ISSO?
+    */
 }
