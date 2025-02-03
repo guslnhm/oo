07a71030784a0e45136aa3c9b6b0e634eb64c245
Gustavo Linhares <143884487+guslnhm@users.noreply.github.com>
2025-02-03 10:51:30 -0300

    criando a medida

---
+++ aula 9 calculadora completa/Medida.java
@@ -0,0 +1,9 @@
+public class Medida{
+    private float valor;
+    private String unidade;
+    
+    public Medida(float valor, String unidade){
+        this.valor = valor;
+        this.unidade = unidade;
+    }
+}
