d527e0cd28e8d3743e064d4924716305b82ecd61
Gustavo Linhares <143884487+guslnhm@users.noreply.github.com>
2025-02-03 12:05:10 -0300

    Update Medida.java

--- aula 9 calculadora completa/Medida.java
+++ aula 9 calculadora completa/Medida.java
@@ -1,9 +1,14 @@
 public class Medida{
-    private float valor;
-    private String unidade;
+    protected float valor;
+    protected String unidade;
     
     public Medida(float valor, String unidade){
         this.valor = valor;
         this.unidade = unidade;
+    }
+    
+    @Override
+    public String toString(){
+        return valor + " " + unidade;
     }
 }
