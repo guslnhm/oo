799faf470f27afa51d07708d10fe7241799d8f8d
Gustavo Linhares <143884487+guslnhm@users.noreply.github.com>
2025-02-05 16:55:50 -0300

    Update Disciplina.java

--- cadastro turmas e alunos/Disciplina.java
+++ cadastro turmas e alunos/Disciplina.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Disciplina{
-    String nome;
-    String codigo;
-    int carga_horaria;
+    
+    private String nome;
+    private String codigo;
+    private int carga_horaria;
+    private Scanner sc = new Scanner(System.in);
+    private static List<Turma> turmas = new ArrayList<>();
+    
+    public Disciplina(String nome){
+        this.nome = nome;
+    }
+    
+    public void adicionar_turma(Turma t){
+        turmas.add(t);
+        for(Turma tt: turmas){
+            System.out.println(tt);
+        }
+    }
+    
 }
