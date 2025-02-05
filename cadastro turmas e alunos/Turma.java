ac2ae202afede2347ab42578b422c66b5c3ba94f
Gustavo Linhares <143884487+guslnhm@users.noreply.github.com>
2025-02-05 16:55:23 -0300

    Update Turma.java

--- cadastro turmas e alunos/Turma.java
+++ cadastro turmas e alunos/Turma.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Turma{
-    Disciplina disciplina;
-    Professor professor;
-    Aluno[] alunos = new Aluno[40];
-    String sala;
-    String horario;
+    
+    private Disciplina disciplina;
+    private Professor professor;
+    private Aluno[] alunos = new Aluno[40];
+    private String sala;
+    private String horario;
+    private Scanner sc = new Scanner(System.in);
+    private String codigo;
+    
+    public Turma(){
+        System.out.print("Informe o nome da disciplina\n>> ");
+        disciplina = new Disciplina(sc.nextLine());
+        System.out.print("Qual sala essa turma ocupa?\n>> ");
+        sala = sc.nextLine();
+        System.out.print("Qual o horario dessa turma?\n>> ");
+        horario = sc.nextLine();
+        System.out.print("Qual o codigo da turma?\n>> ");
+        codigo = sc.nextLine();
+        /*E O PROFESSOR?*/
+    }
+    
+    @Override
+    public String toString(){
+        return sala + " " + horario;
+    }
 }
