f7ef198aac2c07a65e7c24e005e58c5f7ae6d589
Gustavo Linhares <143884487+guslnhm@users.noreply.github.com>
2025-02-05 16:55:37 -0300

    Update Professor.java

--- cadastro turmas e alunos/Professor.java
+++ cadastro turmas e alunos/Professor.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Professor extends Pessoa{
-    String matricula;
-    String titulacao;
+    private String matricula;
+    private String titulacao;
+    private Scanner sc = new Scanner(System.in);
     
     public void setNome(String nome){
         this.nome = nome;
@@ -12,6 +15,24 @@ public class Professor extends Pessoa{
     
     public void setEmail(String email){
         this.email = email;
+    }
+    
+    public Professor(){
+        System.out.print("Informe o nome do professor\n>> ");
+        nome = sc.nextLine();
+        System.out.print("Informe a matricula do professor\n>> ");
+        matricula = sc.nextLine();
+        System.out.print("Informe a titulacao do professor\n>> ");
+        titulacao = sc.nextLine();
+        System.out.print("Informe o telefone do professor\n>> ");
+        telefone = sc.nextLine();
+        System.out.print("Informe o email do professor\n>> ");
+        email = sc.nextLine();
+    }
+    
+    @Override
+    public String toString(){
+        return "NOME: " + nome;
     }
     
 }
